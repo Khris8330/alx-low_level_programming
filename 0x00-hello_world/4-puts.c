a73e8b9a6c34800c9392c2b3c58530e4cdee0702
Umezinwa <kristenumezinwa@gmail.com>
2022-09-08 03:58:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,37 @@
-#include <studio.h>
+/*
+ *
+ *  * File: 4-puts.c
+ *
+ *   * Auth: Brennan D Baraban <375@holbertonschool.com>
+ *
+ *    */
+
+
+
+#include <stdio.h>
+
+
 
 /**
  *
- * * main - entry point 
+ *  * main - Prints "Programming is like building a multilingual
  *
- * *
+ *   *                puzzle, followed by a new line.
  *
- * * Return: 0 after printing the function
+ *    *
  *
- * */
+ *     * Return: Always 0.
+ *
+ *      */
 
 int main(void)
 
-	(puts("\"Programming is like building a multilingual puzzle");
+{
 
-	 return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+
+
+			return (0);
+
+}
