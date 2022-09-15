0ed07ae2e0647486d0dabe5dad2dd93ba64e4121
Umezinwa <kristenumezinwa@gmail.com>
2022-09-15 16:39:02 -0700

    this is stressing me

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,7 @@
 
 /**
  * print_number - Prints a number
- * @n: The number to print
+ * @n: The integer to be printed
  */
 
 void print_number(int n)
@@ -15,9 +15,10 @@ void print_number(int n)
 		putchar('-');
 		num = -num;
 	}
-	if (num > 9)
-	{
+
+	if ((num / 10) > 0)
 		print_number(num / 10);
-	}
-	_putchar(num % 10 + '0');
+
+
+	_putchar((num % 10 + '0');
 }
