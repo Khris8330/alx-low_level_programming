8ef054fc2bf3e7bd7e2b8d3c7f56fa1c31b40362
Umezinwa <kristenumezinwa@gmail.com>
2022-09-15 14:38:08 -0700

    I love you Esther

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,11 +6,11 @@
  * Return: 1 if it is, 0 otherwise
  */
 
-Return: 1 if it is, 0 otherwise
+int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
 	{
-		return (1);
+	return (1);
 	}
 
 	return (0);
