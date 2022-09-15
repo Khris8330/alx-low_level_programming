a9fe062bad44acc4e9b216d87a5e903e8bee9828
Umezinwa <kristenumezinwa@gmail.com>
2022-09-15 14:15:12 -0700

    I love you babe

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked
- *
- * Return: 1 if character is uppercase, 0 otherwise.
+ * _isupper - checks if a  character is uppercase or not
+ * @c: The character to be tested
+ * Return: 1 whether it is, 0 otherwise
  */
 
 int _isupper(int c)
