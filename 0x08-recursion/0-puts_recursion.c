06818dde1fdecaf91ee7fec27dd39169dc3105b6
Khris8330 <111049690+Khris8330@users.noreply.github.com>
2022-09-28 10:20:48 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _putchar - writes the character c to stdout
+ * _puts_recursion - Prints a string followed by a new line
  * @s: string
  *
  * Return: On success 1.
@@ -12,9 +12,8 @@ void _puts_recursion(char *s)
 	if (*s == 0)
 	{
 		_putchar('\n');
-		_puts_recursion(s + 1);
+		return;
 	}
-
-	else
-		_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
